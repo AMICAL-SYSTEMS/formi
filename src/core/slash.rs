60d087a8d3b5d1471564d15d043a60f60c587e8c
hackerbirds <120066692+hackerbirds@users.noreply.github.com>
2025-12-04 21:38:01 -0800

    Fix ordering of numbers in slash (/), which was previously incorrect.

--- src/core/slash.rs
+++ src/core/slash.rs
@@ -1,13 +1,31 @@
-use crate::error::RuntimeError;
+//! 6.1.0230 /
+//! slash CORE
+//!
+//!     ( n1 n2 -- n3 )
+//!
+//! Divide n1 by n2, giving the single-cell quotient n3. An ambiguous
+//! condition exists if n2 is zero. If n1 and n2 differ in sign,
+//! the implementation-defined result returned will be the same as
+//! that returned by either the phrase >R S>D R> FM/MOD SWAP DROP or
+//! the phrase >R S>D R> SM/REM SWAP DROP .
+//!
+//! AMICAL NOTE: Returns a runtime error if n2 is zero.
+//!
+//! [`https://www.taygeta.com/forth/dpans6.htm#6.1.0230`]
+
+use crate::{
+    error::RuntimeError,
+    types::{Cell, Number},
+};
 
 crate::define_word!(Slash, "/", |it, _tks| {
-    let a = it.pop_last_stack()?;
-    let b = it.pop_last_stack()?;
+    let n2: Number = it.pop_last_stack()? as Number;
+    let n1: Number = it.pop_last_stack()? as Number;
 
-    if b == 0 {
+    if n2 == 0 {
         return Err(RuntimeError::DivisionByZero);
     } else {
-        it.stack.push(a / b);
+        it.stack.push((n1 / n2) as Cell);
     }
 
     Ok(())
