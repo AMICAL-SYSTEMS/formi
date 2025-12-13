249ac774db269868367cddce18a40eaa2d224be6
hackerbirds <120066692+hackerbirds@users.noreply.github.com>
2025-12-13 12:46:17 -0800

    Fix token for DUP word

--- src/core/dupe.rs
+++ src/core/dupe.rs
@@ -8,7 +8,7 @@
 //! [https://www.taygeta.com/forth/dpans6.htm#6.1.1290]
 
 use crate::error::RuntimeError;
-crate::define_word!(Dupe, "DUPE", |it, _tks| {
+crate::define_word!(Dupe, "DUP", |it, _tks| {
     it.stack
         .push(it.stack.peek().ok_or(RuntimeError::EmptyStack)?);
 
